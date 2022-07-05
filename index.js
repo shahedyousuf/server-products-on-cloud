495bec27ca2a4f304c3f4d4a34de86614ae31e2c
shahedyousuf <smshahed.yousuf@gmail.com>
2022-07-05 10:24:09 +0200

    post api

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ async function run() {
             const product = await productCollection.findOne(query);
             res.send(product);
         })
+
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        })
     }
     finally {
 
